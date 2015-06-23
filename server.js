d07a22abd0e52c2ad1e7e73dac2c70daa25d07f3
simoncrabtree <simon@crabsticks.net>
2015-06-23 11:00:08 +0100

    Dont use more dynamites than allowed

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ var server = new Hapi.Server();
 var fs = require('fs');
 fs.writeFile('hackday.csv')
 
-var replies = ['ROCK','PAPER','SCISSORS','DYNAMITE','WATERBOMB'];
+var replies = ['WATERBOMB','ROCK','PAPER','SCISSORS','DYNAMITE'];
 var dynamiteCount = 0;
+var min=1;
+var max=5;
 
 server.connection({ port: process.env.port });
 
@@ -30,10 +32,14 @@ server.route({method: 'POST', path: '/start', config : {payload: {parse: true}},
 }});
 
 server.route({method: 'GET', path: '/move', handler: function (request, reply) {
-	var max=4;
-	var min=0;
 	var no = Math.floor(Math.random() * (max - min)) + min
 	var ourmove = replies[no];
+	if(ourmove === "DYNAMITE"){
+		dynamiteCount--;
+		if(dynamiteCount === 0)
+			max = 4;
+	}
+
 	var log = Date.now() + "," + "ourmove" + "," +  ourmove;
 	console.log(log);
 	fs.writeFileSync('hackday.csv', log);
